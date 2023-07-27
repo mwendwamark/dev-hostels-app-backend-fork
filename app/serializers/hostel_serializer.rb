158fcf1a5c8e75bd5f7bc27036547c79a348c803
sheringugi <sheri.ngugi17@gmail.com>
2023-07-27 15:38:34 +0300

    Hostel model created

---
+++ app/serializers/hostel_serializer.rb
@@ -0,0 +1,3 @@
+class HostelSerializer < ActiveModel::Serializer
+  attributes :id, :room_type, :total_occupancy, :total_bedrooms, :total_bathrooms, :total_beds, :summary, :address, :has_tv, :has_kitchen, :has_air_conditioner, :has_internet, :has_study_room, :has_meals, :price, :published_at, :user_id, :latitude, :longitude
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Hostel.create!(room_type: "private room", total_occupancy: 1, total_bedrooms: 1, total_bathrooms: 1, total_beds: 1, summary: "This is an exclusive room for privacy", address: "Mara Court, Ngong Road", has_tv: true, has_kitchen: true, has_air_conditioner: true, has_internet: true, has_study_room: true, has_meals: false, price: 28500, published_at: Time.zone.now , user_id: 1, latitude: "1.3562° S", longitude: "36.6688° E")
