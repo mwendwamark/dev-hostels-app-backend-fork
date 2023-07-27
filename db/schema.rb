e1466bf11e74e05a4f4d30067870059032577c65
sheringugi <sheri.ngugi17@gmail.com>
2023-07-28 01:51:30 +0300

    Update seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_27_093753) do
-
+ActiveRecord::Schema.define(version: 2023_07_27_104122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "hostels", force: :cascade do |t|
+    t.string "room_type"
+    t.integer "total_occupancy"
+    t.integer "total_bedrooms"
+    t.integer "total_bathrooms"
+    t.integer "total_beds"
+    t.string "summary"
+    t.string "address"
+    t.boolean "has_tv"
+    t.boolean "has_kitchen"
+    t.boolean "has_air_conditioner"
+    t.boolean "has_internet"
+    t.boolean "has_study_room"
+    t.boolean "has_meals"
+    t.integer "price"
+    t.datetime "published_at"
+    t.integer "user_id"
+    t.string "latitude"
+    t.string "longitude"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
-  create_table "reviews", force: :cascade do |t|
-    t.integer "reservation_id"
-    t.integer "ratings"
-    t.string "comments"
   create_table "reservations", force: :cascade do |t|
     t.integer "user_id"
     t.integer "hostel_id"
@@ -28,6 +45,14 @@ ActiveRecord::Schema.define(version: 2023_07_27_093753) do
     t.date "end_date"
     t.decimal "price"
     t.decimal "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "reservation_id"
+    t.integer "ratings"
+    t.string "comments"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
