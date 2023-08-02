7a8b98aa9ab23bf0e042ddfd290cfd6d4a33d8b3
sheringugi <sheri.ngugi17@gmail.com>
2023-08-02 10:13:03 +0300

    Resolve reset password bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,32 +2,35 @@ class UsersController < ApplicationController
   skip_before_action :authorize, only: [:create, :update, :destroy, :show, :index]
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
   def index
     users = User.all
     render json: users
   end
+
   def create
     user = User.create!(user_params)
     session[:user_id] = user.id
     render json: user
   end 
 
-
   def update
     user = User.find(params[:id])
     user.update!(user_params)
     render json: user, status: :accepted
   end
+
   def show
     user = User.find(params[:id])
     render json: user, status: :ok
     render json: @current_user
   end
+
   def destroy
     user = User.find(params[:id])
     user.destroy
     head :no_content
-
+  end
 
   def changePass
     user = User.find_by(email: params[:email])
@@ -37,12 +40,15 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.permit(:id, :user, :first_name, :last_name, :email, :phone_number, :password, :password_confirmation)
   end
+
   def render_not_found_response
     render json: { errors: ["User Not Found"] }, status: :not_found
   end
+
   def render_unprocessable_entity(exception)
     render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
   end
