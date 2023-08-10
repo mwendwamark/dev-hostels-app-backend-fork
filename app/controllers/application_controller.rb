f49016180964a3b90ee3c52af935bbdb74fd6b05
sheringugi <sheri.ngugi17@gmail.com>
2023-08-11 00:32:10 +0300

    resolve JWT bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,5 @@
 class ApplicationController < ActionController::API
-  # include JsonWebToken
-  # before_action :authenticate_request
-
-  # private
-
-  # def authenticate_request
-  #   header = request.headers["Authorization"]
-  #   header = header.split(" ").last if header
-  #   decoded = jwt_decode(header)
-  #   @curent_user = User.find(decoded[:user_id])
-  # end
+  
 
   # Without using JWT
   include ActionController::Cookies
