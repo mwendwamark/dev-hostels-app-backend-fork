a49b5049400c5f7776bbd38af933371acabdc0b8
sheringugi <sheri.ngugi17@gmail.com>
2023-08-10 23:29:08 +0300

    Configure for Render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,13 +1,12 @@
-#!/usr/bin/env bash
-# exit on error
+
 set -o errexit
 
-# builds the front end code
+# Build commands for front end to create the production build
 rm -rf public
 npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
-# builds the back end code
-# bundle install
-# bundle exec rake db:migrate
-# bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only
+# Build commands for back end
+bundle install
+# bundle exec rake db:migrate 
+# bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
